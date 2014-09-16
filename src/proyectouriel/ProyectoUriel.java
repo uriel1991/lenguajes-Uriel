dc07c98d96f1cf89d77d699ff75288c14a642be8
uriel1991 <jorgeuriel850@gmail.com>
2014-09-15 19:41:45 -0500

    Update ProyectoUriel.java

--- src/proyectouriel/ProyectoUriel.java
+++ src/proyectouriel/ProyectoUriel.java
@@ -16,7 +16,7 @@ public class ProyectoUriel {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        System.out.println("Mi primer programa");
+        System.out.println("Mi primer programa cambio 2");
     }
     
 }
