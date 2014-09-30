4464ecd67dbc61ea960b52afbf70eb1d0eb2ba04
T <T@PC244>
2014-09-29 19:50:32 -0500

    Mi primer subida

---
+++ src/proyectosistemadepago/Pago.java
@@ -0,0 +1,24 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package proyectosistemadepago;
+    /*papa*/
+public class Pago {
+    
+    /*Mama*/
+    public Pago(){
+        System.out.println(" a ver si es cierto que aparezco ");
+        
+    }
+    
+
+public float hacerPago(int horasTrabajadas, float sueldo){
+    float pago=horasTrabajadas*sueldo;
+    return pago;
+}
+        
+    
+}
